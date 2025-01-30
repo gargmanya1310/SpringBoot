8914164d9dbe8a57b66873c57547e8a2f5c1164e
gargmanya1310 <gargmanya1310@gmail.com>
2025-01-30 16:27:44 +0530

    Test injections: constructor,setter,field

--- src/main/java/com/example/demo/enterprise/web/MyWebController.java
+++ src/main/java/com/example/demo/enterprise/web/MyWebController.java
@@ -1,5 +1,4 @@
-package com.example.demo.enterprise;
-
+package com.example.demo.enterprise.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +9,7 @@ import java.util.List;
 public class MyWebController
 {
     @Autowired
+
     private BusinessService businessService;
     public long returnValueFromBusinessService()
     {
@@ -20,21 +20,43 @@ public class MyWebController
 @Component
 class BusinessService
 {
+
+
     @Autowired
-    private DataService dataService;
+    public BusinessService(DataService dataService)
+    {
+        System.out.println("Constuctor injected");
+        this.dataService = dataService;
+    }
+
+
+    private DataService dataService;            //dependency injection using reflection i.e no constructor or anything used
+
+//    public void setDataService(DataService dataService)
+//    {
+//        System.out.println("setter injection");
+//        this.dataService = dataService;
+//    }
 
     public long calculateSum()
     {
-        List<Integer> data = dataService.getData();
+        List<Integer> data= dataService.getData();
         return data.stream().reduce(Integer::sum).get(); //functional programming
     }
 }
 
+
 @Component
-class DataService
+ class DataService
 {
     public List<Integer> getData()
     {
         return Arrays.asList(10,20,30,40);
     }
 }
+
+
+
+
+
+

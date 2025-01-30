e61fed2c0c985618196f6ec5fc27091e2f4d3144
gargmanya1310 <gargmanya1310@gmail.com>
2025-01-30 12:48:07 +0530

    Test Business enterprise application working with different classes but under 1 file

--- src/main/java/com/example/demo/enterprise/MyWebController.java
+++ src/main/java/com/example/demo/enterprise/MyWebController.java
@@ -1,12 +1,40 @@
 package com.example.demo.enterprise;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class MyWebController
 {
+    @Autowired
+    private BusinessService businessService;
     public long returnValueFromBusinessService()
     {
-        return 200;
+        return businessService.calculateSum();
+    }
+}
+
+@Component
+class BusinessService
+{
+    @Autowired
+    private DataService dataService;
+
+    public long calculateSum()
+    {
+        List<Integer> data = dataService.getData();
+        return data.stream().reduce(Integer::sum).get(); //functional programming
+    }
+}
+
+@Component
+class DataService
+{
+    public List<Integer> getData()
+    {
+        return Arrays.asList(10,20,30,40);
     }
 }
